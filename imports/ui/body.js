2cc05ef0b1349ed3dc952fe3faedf2863399c9e0
ikhsanalatsary <ikhsan.alatsary@yahoo.com>
2016-04-26 16:53:56 +0700

    Add forms and events bookmarks

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -6,12 +6,32 @@ import { Bookmarks } from '../api/bookmarks.js';
 import './body.html';
 
 Template.body.helpers({
-  // bookmarks: [
-  //   {title: "Meteor", url: "https://www.meteor.com/"},
-  //   {title: "AngularJS", url: "https://angularjs.org/"},
-  //   {title: "Google", url: "https://google.com/"},
-  // ]
   bookmarks() {
     return Bookmarks.find({});
+  }
+});
+
+Template.body.events({
+  'submit .add-bookmark'(event) {
+    event.preventDefault();
+
+    const target = event.target;
+    let title = target.title.value;
+    let url = target.url.value;
+
+    if (title === '' || url === '') {
+      throw new Meteor.Error('All field required');
+      return;
+    }
+
+    Bookmarks.insert({
+      title,
+      url,
+      createdAt: new Date()
+    });
+
+    target.title.value = '';
+    target.url.value = '';
+
   }
 });
